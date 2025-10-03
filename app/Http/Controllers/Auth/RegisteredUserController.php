187a917d9dc4d67d29a0a93f1a2fe7c30a1c1b83
Ayas Widyatmaka <ahyasw@gmail.com>
2025-10-04 01:51:11 +0700

    adding migration, add user status when register new one

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'status' => 1 //active user
         ]);
 
         event(new Registered($user));
